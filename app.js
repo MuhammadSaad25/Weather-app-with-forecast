0d25ae054183f1ac4dfbcd30248c29a8c0da2927
Muhammad Saad <99634425+MuhammadSaad25@users.noreply.github.com>
2022-08-28 18:03:52 +0500

    create dives

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // creating HTML with DOM
 
+
+let container = document.querySelector("#container");
+
+//____________________________________________________________________________________
+
 // div class currant info 
 let currentInfo = document.createElement("div");
 currentInfo.setAttribute("class", "current_info");
@@ -17,15 +22,17 @@ container.appendChild(currentInfo);
 // input.setAttribute("placeholder", "Enter City Name");
 // form.appendChild(input);
 
+//_________________________________________________________________________________________
+
 // div class date_container
 let dateContainer = document.createElement("div");
-dateContainer.setAttribute("class","date_container");
+dateContainer.setAttribute("class", "date_container");
 currentInfo.appendChild(dateContainer);
 
 // div class time id time
 let time = document.createElement("div")
-time.setAttribute("class","time");
-time.setAttribute("id","time");
+time.setAttribute("class", "time");
+time.setAttribute("id", "time");
 dateContainer.appendChild(time)
 let timeSet = document.createTextNode("12:00")
 time.appendChild(timeSet)
@@ -37,21 +44,23 @@ ampm.appendChild(ampmSet)
 
 // div class date id date
 let date = document.createElement("div")
-date.setAttribute("class","date");
-date.setAttribute("id","date");
+date.setAttribute("class", "date");
+date.setAttribute("id", "date");
 dateContainer.appendChild(date)
 let dateSet = document.createTextNode(`Sunday, 28 August 2022`)
 date.appendChild(dateSet)
 
+//________________________________________________________________________________
+
 // div class others id currentWeatherItems
 let currentWeatherItems = document.createElement("div")
-currentWeatherItems.setAttribute("class","others");
-currentWeatherItems.setAttribute("id","currentWeatherItems");
+currentWeatherItems.setAttribute("class", "others");
+currentWeatherItems.setAttribute("id", "currentWeatherItems");
 dateContainer.appendChild(currentWeatherItems)
 
 // div class weather_item
 let WeatherItem = document.createElement("div")
-WeatherItem.setAttribute("class","weather_item");
+WeatherItem.setAttribute("class", "weather_item");
 currentWeatherItems.appendChild(WeatherItem)
 
 // p1 p2  
@@ -66,7 +75,7 @@ pra2.appendChild(pra2c1)
 
 // div class weather_item
 let WeatherItem2 = document.createElement("div")
-WeatherItem2.setAttribute("class","weather_item");
+WeatherItem2.setAttribute("class", "weather_item");
 currentWeatherItems.appendChild(WeatherItem2)
 
 // p3 p4 
@@ -81,7 +90,7 @@ pra4.appendChild(pra4c1)
 
 // div class weather_item
 let WeatherItem3 = document.createElement("div")
-WeatherItem3.setAttribute("class","weather_item");
+WeatherItem3.setAttribute("class", "weather_item");
 currentWeatherItems.appendChild(WeatherItem3)
 
 // p5 p6 
@@ -93,4 +102,71 @@ let pra6 = document.createElement("div")
 WeatherItem3.appendChild(pra6)
 let pra6c1 = document.createTextNode(`30%`)
 pra6.appendChild(pra6c1)
+
+//____________________________________________________________________
+
+// div class place_container
+let placeContainer = document.createElement("div")
+placeContainer.setAttribute("class", "place_container")
+currentInfo.appendChild(placeContainer)
+
+// div class time_zone id timeZone
+let timeZone = document.createElement("div")
+timeZone.setAttribute("class", "time_zone")
+timeZone.setAttribute("id", "timeZone")
+placeContainer.appendChild(timeZone)
+let timeZoneSet = document.createTextNode(`Asia`)
+timeZone.appendChild(timeZoneSet)
+
+
+// div class country id country
+let country = document.createElement("div")
+country.setAttribute("class", "country")
+country.setAttribute("id", "country")
+placeContainer.appendChild(country)
+let counntrySet = document.createTextNode(`Pak`)
+country.appendChild(counntrySet)
+
+
+//_____________________________________________________________________________
+
+// div class future-forecast id futureForecast
+let futureForecast = document.createElement("div")
+futureForecast.setAttribute("class", "future_forecast")
+futureForecast.setAttribute("id", "futureForecast")
+container.appendChild(futureForecast)
+
+// div class today id todayTemp
+let todayTemp = document.createElement("div")
+todayTemp.setAttribute("class", "today")
+todayTemp.setAttribute("id", "todayTemp")
+futureForecast.appendChild(todayTemp)
+
+// img class w-icon
+let imgIcon = document.createElement("img")
+imgIcon.setAttribute("src", "")
+imgIcon.setAttribute("alt", "weather icon")
+imgIcon.setAttribute("class", "w_icon")
+todayTemp.appendChild(imgIcon)
+
+// diz class day
+let day = document.createElement("div")
+day.setAttribute("class", "day")
+todayTemp.appendChild(day)
+let daySet = document.createTextNode(`Monday`)
+day.appendChild(daySet)
+
+// div class temp
+let tempn = document.createElement("div")
+tempn.setAttribute("class", "temp")
+todayTemp.appendChild(tempn)
+let temSetN = document.createTextNode(`Night - 25 c`)
+tempn.appendChild(temSetN)
+
+// div class temp
+let tempd = document.createElement("div")
+tempd.setAttribute("class", "temp")
+todayTemp.appendChild(tempd)
+let temSetd= document.createTextNode(`day - 25 c`)
+tempd.appendChild(temSetd)
 
